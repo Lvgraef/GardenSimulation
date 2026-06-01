01885c96bf15456d77a80c333146dc7fcb5978f0
lvgraef <86008039+Lvgraef@users.noreply.github.com>
2026-06-01 20:58:02 +0200

    Zoom

--- Assets/scripts/CameraManager.cs
+++ Assets/scripts/CameraManager.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class CameraManager : MonoBehaviour
 {
@@ -13,11 +17,14 @@ public class CameraManager : MonoBehaviour
     }
 
     [SerializeField] private InputAction rotate;
-
+    [SerializeField] private InputAction zoom;
+    
     [SerializeField] private Origin origin;
 
     [SerializeField] private Transform mainCamera;
 
+    private const float MinZoom = 2;
+    private const float MaxZoom = 50;
 
     void Start()
     {
@@ -26,13 +33,23 @@ public class CameraManager : MonoBehaviour
         transform.rotation = Quaternion.Euler(origin.originRotation);
 
         rotate.Enable();
+        zoom.Enable();
     }
 
     void Update()
     {
+        // rotation
         var rotation = rotate.ReadValue<Vector2>();
 
         transform.Rotate(0, rotation.x * Time.deltaTime, 0, Space.World);
         transform.Rotate(rotation.y * Time.deltaTime, 0, 0, Space.Self);
+        
+        // zoom
+        var zoomAmount = zoom.ReadValue<float>();
+
+        if (Math.Abs(mainCamera.localPosition.z + zoomAmount) < MaxZoom && Math.Abs(mainCamera.localPosition.z + zoomAmount) > MinZoom)
+        {
+            mainCamera.Translate(0, 0, zoomAmount);
+        }
     }
 }
