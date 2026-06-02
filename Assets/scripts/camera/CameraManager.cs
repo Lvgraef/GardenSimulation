9a429fc04afdee5ebd0d36dc4ce3a6fea19dcb90
lvgraef <86008039+Lvgraef@users.noreply.github.com>
2026-06-02 08:18:48 +0200

    Added switch camera button

--- Assets/scripts/camera/CameraManager.cs
+++ Assets/scripts/camera/CameraManager.cs
@@ -1,10 +1,23 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace camera
 {
     public class CameraManager : MonoBehaviour
     {
-        
-        
+        public bool Is2D { get; private set; }
+
+        [SerializeField] private GameObject camera3D;
+        [SerializeField] private GameObject camera2D;
+
+        [SerializeField] private TMP_Text buttonText;
+
+        public void SwitchCameraView()
+        {
+            Is2D = !Is2D;
+            camera2D.SetActive(Is2D);
+            camera3D.SetActive(!Is2D);
+            buttonText.text = Is2D ? "3D" : "2D";
+        }
     }
 }
