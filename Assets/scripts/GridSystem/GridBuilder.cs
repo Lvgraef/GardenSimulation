99f9330523672015df7b03b3cffa6f0b371cf4de
elidev14 <ek.asiamah@student.avans.nl>
2026-06-03 20:44:29 +0200

    Created gridsystem

---
+++ Assets/scripts/GridSystem/GridBuilder.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+namespace GridSystem
+{
+    public class GridBuilder : MonoBehaviour
+    {
+          private Tile[,] tiles;
+
+
+            [SerializeField] private int width;
+            [SerializeField] private int height;
+
+            private float tileSize = 1f;
+            
+            public void RemoveTile(int x, int y)
+            {
+                tiles[x, y].ClearMaterial();
+            }
+
+            /// <summary>
+            /// Places the material on the grid
+            /// </summary>
+            /// <param name="material"></param>
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            public void PlaceMaterial(Material material, int x, int y)
+            {
+                tiles[x, y].SetMaterial(material);
+            }
+
+            /// <summary>
+            /// Returns the Material object
+            /// </summary>
+            /// <param name="x"></param>
+            /// <param name="y"></param>
+            /// <returns></returns>
+            public Material GetMaterial(int x, int y)
+            {
+                return tiles[x, y].GetMaterial();
+            }
+        
+            
+            /// <summary>
+            /// Generates the grid
+            /// </summary>
+            private void GenerateGrid()
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        tiles[x, y] = new Tile ();
+                    }
+                }
+            }
+            
+            /// <summary>
+            /// Draw lines in unity
+            /// </summary>
+            private void OnDrawGizmos()
+            {
+                Gizmos.color = Color.black;
+                if (width <= 0 || height <= 0) return;
+                Vector3 origin = transform.position;
+
+                for (int y = 0; y <= height; y++)
+                {
+                    Vector3 start = origin + new Vector3(0, 0.01f, y * tileSize);
+                    Vector3 end = origin + new Vector3(width * tileSize, 0.01f, y * tileSize);
+                    Gizmos.DrawLine(start, end);
+                }
+
+                for (int x = 0; x <= width; x++)
+                {
+                    Vector3 start = origin + new Vector3(x * tileSize, 0.01f, 0);
+                    Vector3 end = origin + new Vector3(x * tileSize, 0.01f, height * tileSize);
+                    Gizmos.DrawLine(start, end);
+                }
+            }
+
+
+            void Start()
+            {
+                tiles = new Tile[width, height];
+                GenerateGrid();
+            }
+    }
+}
