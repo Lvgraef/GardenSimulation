cbf73626fae0eed436b2f4f7181a5dcf80ae2fa8
elidev14 <ek.asiamah@student.avans.nl>
2026-06-03 20:44:29 +0200

    Created gridsystem

---
+++ Assets/scripts/GridSystem/MaterialData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace GridSystem
+{
+    /// <summary>
+    /// This is the metadata for the materials
+    /// </summary>
+    [CreateAssetMenu(menuName = "Data/MaterialData", order = 0)]
+    public class MaterialData : ScriptableObject
+    {
+        //Ideas for this is a :  description for material, name of material
+        
+    }
+}
