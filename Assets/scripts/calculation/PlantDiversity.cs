12717e653df61edf13215d92c57a0935b22e3785
lvgraef <86008039+Lvgraef@users.noreply.github.com>
2026-06-05 21:58:36 +0200

    Rekenmodel

---
+++ Assets/scripts/calculation/PlantDiversity.cs
@@ -0,0 +1,11 @@
+﻿namespace calculation
+{
+    public enum PlantDiversity
+    {
+        None, // Ik h eb geen planten in mijn tuin
+        Little, // 1 tot en met 3 soorten
+        Medium, // 4 tot en met 10 soorten
+        High, // 11 tot en met 25 soorten
+        Highest // meer dan 25 soorten
+    }
+}
