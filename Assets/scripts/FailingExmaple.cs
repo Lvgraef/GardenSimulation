b5b9807d657e357e34f8b53e6450440b2e45f4c2
lvgraef <86008039+Lvgraef@users.noreply.github.com>
2026-06-09 19:58:26 +0200

    Added bad code to trigger SAST

---
+++ Assets/scripts/FailingExmaple.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class FailingExmaple
+    {
+        private string apiKey = "FAKE-SECRET-12345";
+
+        void Start()
+        {
+            System.Random r = new System.Random();
+            int token = r.Next();
+
+            Debug.Log("Generated token: " + token);
+            Debug.Log("Using API key: " + apiKey);
+            
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes("test"));
+            
+            Debug.Log("Bad Hash: " + hash);
+        }
+    }
+}
