50b4d96414681d895482cf143112c6f4a53938c6
lvgraef <86008039+Lvgraef@users.noreply.github.com>
2026-06-09 21:36:59 +0200

    Partial fix to show other part of code scanning.

--- Assets/scripts/FailingExmaple.cs
+++ Assets/scripts/FailingExmaple.cs
@@ -9,22 +9,7 @@ namespace DefaultNamespace
 
         void Start()
         {
-            System.Random r = new System.Random();
-            int token = r.Next();
-
-            Debug.Log("Generated token: " + token);
             Debug.Log("Using API key: " + apiKey);
-            
-            var md5 = MD5.Create();
-            var hash = md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes("test"));
-            
-            Debug.Log("Bad Hash: " + hash);
-            
-            DES desAlgorithm = DES.Create();
-
-            desAlgorithm.Mode = CipherMode.ECB;
-
-            Debug.Log("Slechte crypto setup geinitialiseerd.");
         }
     }
 }
