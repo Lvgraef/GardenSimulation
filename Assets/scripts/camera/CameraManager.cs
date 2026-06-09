9e5fc2e64d14aa56cbf13f999a848bc816212d7b
elidev14 <ek.asiamah@student.avans.nl>
2026-06-09 21:33:07 +0200

    fixed aligning and changed buttons to icons

--- Assets/scripts/camera/CameraManager.cs
+++ Assets/scripts/camera/CameraManager.cs
@@ -2,6 +2,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 namespace camera
 {
@@ -19,10 +20,13 @@ namespace camera
         private Camera cameraComponent3D;
         [SerializeField]
         private Camera cameraComponent2D;
-
-        [SerializeField] private TMP_Text buttonText;
+        
 
         [SerializeField] private InputAction switchButton;
+        
+        [SerializeField] private Image buttonImage;
+        [SerializeField] private Sprite icon3D;
+        [SerializeField] private Sprite icon2D;
 
         private void Start()
         {
@@ -45,7 +49,10 @@ namespace camera
             Is2D = !Is2D;
             camera2D.SetActive(Is2D);
             camera3D.SetActive(!Is2D);
-            buttonText.text = Is2D ? "3D" : "2D";
+
+            
+            buttonImage.sprite = Is2D ? icon3D : icon2D;
+            // buttonText.text = Is2D ? "3D" : "2D";
         }
 
         public Camera GetCurrentCamera()
