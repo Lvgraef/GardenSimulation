679464dc17a7fd680c02330504d57351974f5e80
lvgraef <86008039+Lvgraef@users.noreply.github.com>
2026-06-11 13:35:27 +0200

    Added actions

--- Assets/scripts/ai/GridAgent.cs
+++ Assets/scripts/ai/GridAgent.cs
@@ -2,6 +2,7 @@
 using gardensettings;
 using GridSystem;
 using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
 using Material = GridSystem.Material;
@@ -14,18 +15,18 @@ namespace ai
         [SerializeField] private GardenSettings gardenSettings;
         [SerializeField] private Material[] randomizedMaterials;
         [SerializeField] private Material buildingMaterial;
-        
+
         private int _preFilled;
 
         private const int MaxWidth = 20;
         private const int MaxHeight = 20;
-        
+
         Dictionary<string, int> materialAreaCounts = new();
-        
+
         private void PreFillGridRandomly(Material material, int count)
         {
             List<(int, int)> emptyTiles = new();
-            
+
             grid.ForEachTile((tile, x, y) =>
             {
                 if (tile is null)
@@ -33,7 +34,7 @@ namespace ai
                     emptyTiles.Add((x, y));
                 }
             });
-            
+
             for (int i = 0; i < count; i++)
             {
                 int index = Random.Range(0, emptyTiles.Count);
@@ -45,7 +46,7 @@ namespace ai
         {
             PreFillGridRandomly(buildingMaterial, Random.Range(0, 50));
         }
-        
+
         private void PreFillMaterials()
         {
             int materialIndex = Random.Range(0, randomizedMaterials.Length);
@@ -58,9 +59,9 @@ namespace ai
         private void PickAreas()
         {
             float bias = 3f;
-            
+
             var size = 0;
-            
+
             grid.ForEachTile((tile, x, y) =>
             {
                 if (tile is null)
@@ -68,14 +69,14 @@ namespace ai
                     size++;
                 }
             });
-            
+
             List<Material> pickableMaterials = new();
-            
+
             foreach (Material mat in randomizedMaterials)
             {
                 pickableMaterials.Add(mat);
             }
-        
+
             for (int i = 0; i < randomizedMaterials.Length; i++)
             {
                 int materialIndex = Random.Range(0, pickableMaterials.Count);
@@ -92,12 +93,12 @@ namespace ai
             gardenSettings.FlyingInsects = Random.Range(0, 2) == 1;
             gardenSettings.Spiders = Random.Range(0, 2) == 1;
             gardenSettings.OtherAnimals = Random.Range(0, 2) == 1;
-            
-            gardenSettings.CompostCleanup = (CompostCleanup) Random.Range(0, 3);
-            gardenSettings.PlantDiversity = (PlantDiversity) Random.Range(0, 5);
-            gardenSettings.Fertilizer = (Fertilizer) Random.Range(0, 3);
+
+            gardenSettings.CompostCleanup = (CompostCleanup)Random.Range(0, 3);
+            gardenSettings.PlantDiversity = (PlantDiversity)Random.Range(0, 5);
+            gardenSettings.Fertilizer = (Fertilizer)Random.Range(0, 3);
         }
-        
+
         public override void OnEpisodeBegin()
         {
             grid.Reset();
@@ -136,14 +137,26 @@ namespace ai
                     }
                 }
             }
-            
+
             sensor.AddObservation(gardenSettings.Birds);
             sensor.AddObservation(gardenSettings.FlyingInsects);
             sensor.AddObservation(gardenSettings.Spiders);
             sensor.AddObservation(gardenSettings.OtherAnimals);
-            sensor.AddObservation((float) gardenSettings.CompostCleanup);
-            sensor.AddObservation((float) gardenSettings.Fertilizer);
-            sensor.AddObservation((float) gardenSettings.PlantDiversity);
+            sensor.AddObservation((float)gardenSettings.CompostCleanup);
+            sensor.AddObservation((float)gardenSettings.Fertilizer);
+            sensor.AddObservation((float)gardenSettings.PlantDiversity);
+        }
+
+        public override void OnActionReceived(ActionBuffers actions)
+        {
+            for (int i = 0; i < MaxWidth; i++)
+            {
+                for (int j = 0; j < MaxHeight; j++)
+                {
+                    int index = i * MaxWidth + j;
+                    grid.PlaceMaterial(randomizedMaterials[actions.DiscreteActions[index]], (i, j));
+                }
+            }
         }
     }
 }
