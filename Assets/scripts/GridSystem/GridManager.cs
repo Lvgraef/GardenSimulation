93c51cfac7a818dac2410609823f0d70f0feafa3
FB99FIRE <151029053+FB99FIRE@users.noreply.github.com>
2026-06-11 03:16:02 +0200

    touch controls
    added touch controlls and the blender objects in an asset folder.
    
    when building on an android phone it works as intenden, when building on an android tablet the "drawing" part of the digital twin wil not work
    
    cleanup
    
    code cleanup and adjusting speeds for movement
    
    Update Simulation.unity
    
    feature: added zoom and single touch check
    
    added functionality for zoom in the 2D view. also added a check for single finger touch in gridmanager so tiles will not be painted when panning, rotating or zooming
    
    fix camera controls
    
    fix camera controls for up down movement
    
    fix data
    
    removed folders that are not supposed to be comitted

--- Assets/scripts/GridSystem/GridManager.cs
+++ Assets/scripts/GridSystem/GridManager.cs
@@ -37,14 +37,66 @@ namespace GridSystem
 
         private void Update()
         {
-            
-            if (Mouse.current.leftButton.isPressed || Touchscreen.current.primaryTouch.press.isPressed)
+            if (Touchscreen.current != null) {
+                int totalTouches = 0;
+
+                foreach (var touch in Touchscreen.current.touches)
+                {
+                    if (touch.press.isPressed)
+                    {
+                        totalTouches++;
+                    }
+                }
+
+                if (totalTouches == 1)
+                {
+                    ShootRay();
+                }
+            }
+            if (Mouse.current.leftButton.isPressed)
             {
                 ShootRay();
             }
         }
 
         private void ShootRay()
+        {
+            if (Touchscreen.current != null) { touchRay(); }
+
+
+            if (Mouse.current != null) { mouseRay(); }
+
+
+        }
+
+        private void touchRay()
+        {
+            if (EventSystem.current.IsPointerOverGameObject()) return;
+
+            var selectedMaterial = menu.SelectedMaterial;
+            Vector2 fingerPosition = Touchscreen.current.position.ReadValue();
+            Plane gridPlane = new Plane(Vector3.up, transform.position);
+            Ray ray = cameraManager.GetCurrentCamera()
+                .ScreenPointToRay(new Vector3(fingerPosition.x, fingerPosition.y, 0));
+            gridPlane.Raycast(ray, out float distance);
+            var intersectPosition = ray.direction * distance + ray.origin;
+            var gridPos = WorldToGrid(intersectPosition);
+            var mat = GetMaterial(gridPos.x, gridPos.y);
+            if (menu.Eraser)
+            {
+                RemoveMaterial((gridPos.x, gridPos.y));
+                GridChangeEvent?.Invoke();
+                return;
+            }
+
+            if (selectedMaterial is null || (mat is not null &&
+                                             mat.name[..selectedMaterial.name.Length] ==
+                                             selectedMaterial.name)) return;
+
+            PlaceMaterial(selectedMaterial, gridPos);
+            GridChangeEvent?.Invoke();
+        }
+        private void mouseRay()
         {
             if (EventSystem.current.IsPointerOverGameObject()) return;
 
@@ -71,6 +123,7 @@ namespace GridSystem
             PlaceMaterial(selectedMaterial, gridPos);
             GridChangeEvent?.Invoke();
         }
+
 
         public void RemoveTile(int x, int y)
         {
--- Assets/scripts/camera/CameraControl3D.cs
+++ Assets/scripts/camera/CameraControl3D.cs
@@ -5,7 +5,6 @@ using UnityEngine.InputSystem;
 using Quaternion = UnityEngine.Quaternion;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
-using System.Linq;
 
 namespace camera
 {
@@ -21,7 +20,9 @@ namespace camera
             public Vector2 originRotation;
             public float originDistance;
         }
-
+        private float rotationSpeedMouse = 0.001f;
+        private float rotationSpeedTouch = 0.1f;
+        private float panSpeedTouch = 0.1f;
         [SerializeField] private InputAction rotate;
         [SerializeField] private InputAction zoom;
         [SerializeField] private InputAction pan;
@@ -33,7 +34,7 @@ namespace camera
         [SerializeField] private Transform mainCamera;
 
         private const float MinZoom = 2;
-        private const float MaxZoom = 50;
+        private const float MaxZoom = 10;
 
         void Start()
         {
@@ -57,23 +58,44 @@ namespace camera
             }
 
             // rotation
-           
-            bool isTouchRotating = Touchscreen.current != null && Touchscreen.current.touches.Count(t => t.press.isPressed) >= 2;
-            bool isMouseRotating = Mouse.current != null && Mouse.current.middleButton.isPressed;
+
+            bool isTouchRotating = false;
+            if (Touchscreen.current != null) {
+                int count = 0;
+                foreach (var r in Touchscreen.current.touches) {
+                    if (r.press.IsPressed()) {
+                        count++;
+                    }
+                }
+                isTouchRotating = count == 2;
+            }
+            bool isMouseRotating = Mouse.current != null && Mouse.current.rightButton.isPressed;
             bool isArrowRotating = Keyboard.current != null &&
             (Keyboard.current.upArrowKey.isPressed ||
              Keyboard.current.downArrowKey.isPressed ||
              Keyboard.current.leftArrowKey.isPressed ||
              Keyboard.current.rightArrowKey.isPressed);
 
-            if (isTouchRotating || isMouseRotating || isArrowRotating) {
+            if (isArrowRotating) {
                 var rotation = rotate.ReadValue<Vector2>() * Time.deltaTime;
                 transform.Rotate(0, rotation.x, 0, Space.World);
                 transform.Rotate(rotation.y, 0, 0, Space.Self);
             }
 
+            if (isMouseRotating)
+            {
+                var rotation = rotate.ReadValue<Vector2>() * rotationSpeedMouse;
+                transform.Rotate(0, rotation.x, 0, Space.World);
+                transform.Rotate(rotation.y, 0, 0, Space.Self);
+            }
+            if (isTouchRotating)
+            {
+                var rotation = rotate.ReadValue<Vector2>() * rotationSpeedTouch;
+                transform.Rotate(0, rotation.x, 0, Space.World);
+                transform.Rotate(rotation.y, 0, 0, Space.Self);
+            }
 
-            
+
 
             if (!EventSystem.current.IsPointerOverGameObject())
             {
@@ -84,33 +106,80 @@ namespace camera
                 {
                     transform.Translate(0, heightAmount, 0, Space.World);
                 }
-                
-                // zoom
-                if (heightAmount == 0)
-                {
-                    var zoomAmount = zoom.ReadValue<float>() * Time.deltaTime;
 
-                    if (Math.Abs(mainCamera.localPosition.z + zoomAmount) < MaxZoom &&
-                        Math.Abs(mainCamera.localPosition.z + zoomAmount) > MinZoom)
+                // zoom
+                bool isKeyboardZooming = Keyboard.current != null &&
+                                  (Keyboard.current.equalsKey.isPressed ||
+                                  Keyboard.current.minusKey.isPressed);
+                                 
+                bool isMouseZooming = Mouse.current != null && Mouse.current.scroll.ReadValue().y != 0;
+                if (isMouseZooming || isKeyboardZooming)
+                {
+                    if (heightAmount == 0)
                     {
-                        mainCamera.localPosition = new Vector3(mainCamera.localPosition.x, mainCamera.localPosition.y,
-                            Math.Clamp(mainCamera.localPosition.z + zoomAmount, -MaxZoom, -MinZoom));
+                        var zoomAmount = zoom.ReadValue<float>() * Time.deltaTime;
+
+                        if (Math.Abs(mainCamera.localPosition.z + zoomAmount) < MaxZoom &&
+                            Math.Abs(mainCamera.localPosition.z + zoomAmount) > MinZoom)
+                        {
+                            mainCamera.localPosition = new Vector3(mainCamera.localPosition.x, mainCamera.localPosition.y,
+                                Math.Clamp(mainCamera.localPosition.z + zoomAmount, -MaxZoom, -MinZoom));
+                        }
                     }
                 }
             }
 
             // pan
-            var panAmount = pan.ReadValue<Vector2>() * Time.deltaTime;
+            bool isTouchPanning = false;
+            if (Touchscreen.current != null)
+            {
+                int count = 0;
+                foreach (var p in Touchscreen.current.touches)
+                {
+                    if (p.press.IsPressed())
+                    {
+                        count++;
+                    }
+                }
+                isTouchPanning = count == 3;
+            }
+            bool isArrowPanning = Keyboard.current != null &&
+                            (Keyboard.current.wKey.isPressed ||
+                             Keyboard.current.aKey.isPressed ||
+                             Keyboard.current.dKey.isPressed ||
+                             Keyboard.current.sKey.isPressed);
+            bool isMiddleMousePanning = Mouse.current != null && Mouse.current.middleButton.isPressed;
 
-            var forward = transform.forward;
-            forward.y = 0;
-            forward.Normalize();
 
-            var right = transform.right;
-            right.y = 0;
-            right.Normalize();
+            if (isArrowPanning || isMiddleMousePanning) {
+                var panAmount = pan.ReadValue<Vector2>() * Time.deltaTime;
 
-            transform.Translate(right * panAmount.x + forward * panAmount.y, Space.World);
+                var forward = transform.forward;
+                forward.y = 0;
+                forward.Normalize();
+
+                var right = transform.right;
+                right.y = 0;
+                right.Normalize();
+
+                transform.Translate(right * panAmount.x + forward * panAmount.y, Space.World);
+            }
+
+            if (isTouchPanning) {
+                var panAmount = pan.ReadValue<Vector2>() * Time.deltaTime;
+
+                var forward = transform.forward;
+                forward.y = 0;
+                forward.Normalize();
+
+                var right = transform.right;
+                right.y = 0;
+                right.Normalize();
+
+                transform.Translate(right * panAmount.x * panSpeedTouch + forward * -panAmount.y * panSpeedTouch, Space.World);
+            }
+
+            
         }
 
         void ResetTransform()
