3be8cbb5906d34db50e8e4d1fb951c048db9f533
FB99FIRE <151029053+FB99FIRE@users.noreply.github.com>
2026-06-15 14:25:22 +0200

    cleanup
    code cleanup and adjusting speeds for movement

--- Assets/scripts/camera/CameraControl2D.cs
+++ Assets/scripts/camera/CameraControl2D.cs
@@ -21,6 +21,7 @@ namespace camera
 
         [SerializeField] private Origin origin;
 
+        [SerializeField] private float panSpeedTouch = 0.5f;
         [SerializeField] private InputAction pan;
         [SerializeField] private InputAction zoom;
         [SerializeField] private InputAction reset;
@@ -51,10 +52,40 @@ namespace camera
                 var zoomAmount = zoom.ReadValue<float>() * Time.deltaTime;
                 cameraComponent.orthographicSize = Math.Clamp(cameraComponent.orthographicSize + zoomAmount, MinZoom, MaxZoom);
             }
-              
+
             // Pan
-            var panAmount = pan.ReadValue<Vector2>() * Time.deltaTime;
-            transform.Translate(panAmount.x, 0, panAmount.y, Space.World);
+            bool isTouchPanning = false;
+            if (Touchscreen.current != null)
+            {
+                int count = 0;
+                foreach (var p in Touchscreen.current.touches)
+                {
+                    if (p.press.IsPressed())
+                    {
+                        count++;
+                    }
+                }
+                isTouchPanning = count == 3;
+            }
+
+            bool isArrowPanning = Keyboard.current != null &&
+                            (Keyboard.current.wKey.isPressed ||
+                             Keyboard.current.aKey.isPressed ||
+                             Keyboard.current.dKey.isPressed ||
+                             Keyboard.current.sKey.isPressed);
+            bool isMiddleMousePanning = Mouse.current != null && Mouse.current.middleButton.isPressed;
+
+            if (isArrowPanning || isMiddleMousePanning)
+            {
+                var panAmount = pan.ReadValue<Vector2>() * Time.deltaTime;
+                transform.Translate(panAmount.x, 0, panAmount.y, Space.World);
+            }
+
+            if (isTouchPanning)
+            {
+                var panAmount = pan.ReadValue<Vector2>();
+                transform.Translate(panAmount.x * panSpeedTouch, 0, panAmount.y * panSpeedTouch, Space.World);
+            }
         }
 
         private void ResetTransform()
