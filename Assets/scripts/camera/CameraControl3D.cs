268557a16083c3b3bef887981efdca3d4a8df36a
FB99FIRE <felix99@hotmail.nl>
2026-06-23 22:46:13 +0200

    clean-fix
    fixed the code for touchscreen handling and cleaned it up

--- Assets/scripts/camera/CameraControl3D.cs
+++ Assets/scripts/camera/CameraControl3D.cs
@@ -1,7 +1,10 @@
 using System;
+using Unity.MLAgents.Sensors;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
 using Quaternion = UnityEngine.Quaternion;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
@@ -20,28 +23,26 @@ namespace camera
             public Vector2 originRotation;
             public float originDistance;
         }
-        private float rotationSpeedMouse = 0.001f;
-        private float rotationSpeedTouch = 0.1f;
-        private float panSpeedTouch = 0.1f;
         [SerializeField] private InputAction rotate;
         [SerializeField] private InputAction zoom;
         [SerializeField] private InputAction pan;
         [SerializeField] private InputAction height;
         [SerializeField] private InputAction reset;
-
         [SerializeField] private Origin origin;
-
         [SerializeField] private Transform mainCamera;
-
-        private const float MinZoom = 2;
-        private const float MaxZoom = 30;
+        [SerializeField] private const float MinZoom = 1;
+        [SerializeField] private const float MaxZoom = 15;
+        [SerializeField] private float rotationSpeedMouse = 0.001f;
+        [SerializeField] private float rotationSpeedTouch = 0.1f;
+        [SerializeField] private float panSpeedTouch = 0.5f;
+        [SerializeField] private float zoomSpeedTouch = 0.05f;
 
         void Start()
         {
             transform.position = origin.originPosition;
             mainCamera.position = new Vector3(mainCamera.position.x, mainCamera.position.y, -origin.originDistance);
             transform.rotation = Quaternion.Euler(origin.originRotation);
-
+            EnhancedTouchSupport.Enable();
             rotate.Enable();
             zoom.Enable();
             pan.Enable();
@@ -56,27 +57,27 @@ namespace camera
             {
                 ResetTransform();
             }
+            panning();
+            rotating();
+            zooming();
+            
+            
+        }
 
-            // rotation
-
-            bool isTouchRotating = false;
-            if (Touchscreen.current != null) {
-                int count = 0;
-                foreach (var r in Touchscreen.current.touches) {
-                    if (r.press.IsPressed()) {
-                        count++;
-                    }
-                }
-                isTouchRotating = count == 2;
-            }
+        //rotate function that handles mouse, touch and keyboard input for rotating the camera
+        private void rotating() {
+            if (EventSystem.current.IsPointerOverGameObject()) { return; }
+            var activeTouches = UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches;
             bool isMouseRotating = Mouse.current != null && Mouse.current.rightButton.isPressed;
+
             bool isArrowRotating = Keyboard.current != null &&
             (Keyboard.current.upArrowKey.isPressed ||
              Keyboard.current.downArrowKey.isPressed ||
              Keyboard.current.leftArrowKey.isPressed ||
              Keyboard.current.rightArrowKey.isPressed);
 
-            if (isArrowRotating) {
+            if (isArrowRotating)
+            {
                 var rotation = rotate.ReadValue<Vector2>() * Time.deltaTime;
                 transform.Rotate(0, rotation.x, 0, Space.World);
                 transform.Rotate(rotation.y, 0, 0, Space.Self);
@@ -88,68 +89,55 @@ namespace camera
                 transform.Rotate(0, rotation.x, 0, Space.World);
                 transform.Rotate(rotation.y, 0, 0, Space.Self);
             }
-            if (isTouchRotating)
-            {
-                var rotation = rotate.ReadValue<Vector2>() * rotationSpeedTouch;
-                transform.Rotate(0, rotation.x, 0, Space.World);
-                transform.Rotate(rotation.y, 0, 0, Space.Self);
+
+            if (activeTouches.Count == 2) {
+                Vector2 avgDeltaTouch =
+                    (activeTouches[0].delta +
+                     activeTouches[1].delta) / 2f;
+                transform.Rotate(0, avgDeltaTouch.x * rotationSpeedTouch, 0, Space.World);
+                transform.Rotate(avgDeltaTouch.y * rotationSpeedTouch, 0, 0, Space.Self);
             }
+        }
 
+        //zoom function for keyboard with = and - keys, mouse scroll wheel and touch pinch gesture for touchscreen
+        private void zooming() {
+            if (EventSystem.current.IsPointerOverGameObject()) { return; }
+            var activeTouches = UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches;
+            var zoomAmount = zoom.ReadValue<float>() * Time.deltaTime;
 
+            if (activeTouches.Count == 2) { 
+                var touch0 = activeTouches[0];
+                var touch1 = activeTouches[1];
 
-            if (!EventSystem.current.IsPointerOverGameObject())
-            {
-                // height
-                var heightAmount = height.ReadValue<float>() * Time.deltaTime;
-
-                if (Math.Abs(mainCamera.localPosition.y + heightAmount) > 0)
-                {
-                    transform.Translate(0, heightAmount, 0, Space.World);
-                }
-
-                // zoom
-                bool isKeyboardZooming = Keyboard.current != null &&
-                                  (Keyboard.current.equalsKey.isPressed ||
-                                  Keyboard.current.minusKey.isPressed);
-                                 
-                bool isMouseZooming = Mouse.current != null && Mouse.current.scroll.ReadValue().y != 0;
-                if (isMouseZooming || isKeyboardZooming)
-                {
-                    if (heightAmount == 0)
-                    {
-                        var zoomAmount = zoom.ReadValue<float>() * Time.deltaTime;
-
-                        if (Math.Abs(mainCamera.localPosition.z + zoomAmount) < MaxZoom &&
-                            Math.Abs(mainCamera.localPosition.z + zoomAmount) > MinZoom)
-                        {
-                            mainCamera.localPosition = new Vector3(mainCamera.localPosition.x, mainCamera.localPosition.y,
-                                Math.Clamp(mainCamera.localPosition.z + zoomAmount, -MaxZoom, -MinZoom));
-                        }
-                    }
-                }
+                float zoomDistance = Vector2.Distance(
+                    touch0.screenPosition, 
+                    touch1.screenPosition
+                    );
+                float previousZoomDistance = Vector2.Distance(
+                    touch0.screenPosition - touch0.delta, 
+                    touch1.screenPosition - touch1.delta
+                    );
+                float zoom = zoomDistance - previousZoomDistance;
+                zoomAmount += zoom * zoomSpeedTouch;
             }
-
-            // pan
-            bool isTouchPanning = false;
-            if (Touchscreen.current != null)
+            if (Math.Abs(mainCamera.localPosition.z + zoomAmount) < MaxZoom &&
+                           Math.Abs(mainCamera.localPosition.z + zoomAmount) > MinZoom)
             {
-                int count = 0;
-                foreach (var p in Touchscreen.current.touches)
-                {
-                    if (p.press.IsPressed())
-                    {
-                        count++;
-                    }
-                }
-                isTouchPanning = count == 3;
+                mainCamera.localPosition = new Vector3(mainCamera.localPosition.x, mainCamera.localPosition.y,
+                    Math.Clamp(mainCamera.localPosition.z + zoomAmount, -MaxZoom, -MinZoom));
             }
+        }
+
+        //pan function for keyboard with WASD, mouse middle button and touch three finger drag gesture for touchscreen
+        private void panning() {
+            if (EventSystem.current.IsPointerOverGameObject()) { return; }
+            var activeTouches = UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches;
             bool isArrowPanning = Keyboard.current != null &&
                             (Keyboard.current.wKey.isPressed ||
                              Keyboard.current.aKey.isPressed ||
                              Keyboard.current.dKey.isPressed ||
                              Keyboard.current.sKey.isPressed);
             bool isMiddleMousePanning = Mouse.current != null && Mouse.current.middleButton.isPressed;
-
 
             if (isArrowPanning || isMiddleMousePanning) {
                 var panAmount = pan.ReadValue<Vector2>() * Time.deltaTime;
@@ -164,22 +152,25 @@ namespace camera
 
                 transform.Translate(right * panAmount.x + forward * panAmount.y, Space.World);
             }
+            if (activeTouches.Count == 3) { 
+                Vector2 avgDeltaTouch =
+                    (activeTouches[0].delta +
+                     activeTouches[1].delta +
+                     activeTouches[2].delta) / 3f;
 
-            if (isTouchPanning) {
-                var panAmount = pan.ReadValue<Vector2>() * Time.deltaTime;
-
-                var forward = transform.forward;
+                Vector3 forward = transform.forward;
                 forward.y = 0;
                 forward.Normalize();
 
-                var right = transform.right;
+                Vector3 right = transform.right;
                 right.y = 0;
                 right.Normalize();
 
-                transform.Translate(right * panAmount.x * panSpeedTouch + forward * -panAmount.y * panSpeedTouch, Space.World);
-            }
+                Vector3 move = (-right * avgDeltaTouch.x + -forward * avgDeltaTouch.y)
+                    * panSpeedTouch * Time.deltaTime;
 
-            
+                transform.position += move;
+            }
         }
 
         void ResetTransform()
